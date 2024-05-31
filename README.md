5b04c6c1795be088832b4f42b37ad2d603ca2ca2
Brandon Corfman <bcorfman@fastmail.fm>
2024-05-31 14:20:10 -0400

    Initial running version

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# PettingZoo-TicTacToe
+# PettingZoo-ConnectFour
+
+PettingZoo ConnectFour example configured with Rye as dependency manager. Tested with MacOS Sonoma and Ubuntu on WSL2.
+
+## Prerequisites
+* At a type `make devinstall` to set up Rye with the appropriate Python version, plus the project-level dependencies in development mode, or just `make install` to set up the code as runnable only.
+
+## To run the example
+* At a command prompt in the project directory, type `make run`.
+* The first time through, the RL model will be trained and then saved to disk. After that, the trained model will be loaded from disk before evaluation.
+* If retraining is desired, delete the model .zip files from the project directory.
+
+## Notes for Visual Studio Code users
+* I've included some extension recommendations that can make your development easier.
+  * [Run On Save](https://marketplace.visualstudio.com/items?itemName=emeraldwalk.RunOnSave)
+  * [Make support and task provider](https://marketplace.visualstudio.com/items?itemName=carlos-algms.make-task-provider)
+* These recommendations will pop up when opening the project inside VSCode.
+* Installing both extensions will
+  * Use the code in `settings.json` to run `make format` and `make lint` on each `File:Save`.
+  * Display available Make targets within the _Makefile Tasks_ sidebar pane and allow them to be run with a mouse click.
--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-from core.hello import hello_world
+from pettingzoo.classic import connect_four_v3
+
+from core.traineval import eval_action_mask, get_latest_policy, train_action_mask
 
 
 def main():
-    hello_world()
+    env_fn = connect_four_v3
+
+    env_kwargs = {}
+
+    # Evaluation/training hyperparameter notes:
+    # 10k steps: Winrate:  0.76, loss order of 1e-03
+    # 20k steps: Winrate:  0.86, loss order of 1e-04
+    # 40k steps: Winrate:  0.86, loss order of 7e-06
+
+    if not get_latest_policy(env_fn):
+        # Train a model against itself (takes ~40 seconds on a laptop CPU).
+        # Train from scratch only if we can't find latest model on disk.
+        # If retraining is desired, delete the model .zip files from
+        # the project directory.
+        train_action_mask(env_fn, steps=41000, seed=0, **env_kwargs)
+
+    print()
+    print("-----------------------------------")
+    print("Eval 100 games against random agent")
+    print("-----------------------------------")
+    # Evaluate 100 games against a random agent (winrate should be ~80%)
+    eval_action_mask(env_fn, num_games=100, render_mode=None, **env_kwargs)
+
+    print()
+    print("------------------------------------")
+    print("Display 2 games against random agent")
+    print("------------------------------------")
+    # Watch two games vs a random agent
+    eval_action_mask(env_fn, num_games=2, render_mode="human", **env_kwargs)
 
 
 if __name__ == "__main__":
